0b7de374ef4a3fc3411aec103c5fc2f0c8d0e1fb
Roman Reinelt <roman.rnlt@gmail.com>
2021-05-04 12:22:19 +0200

    Cancel when available

--- amdshop.py
+++ amdshop.py
@@ -5,7 +5,6 @@ import time
 from bs4 import BeautifulSoup
 import threading
 import subprocess
-import sys
 
 rx6800xt = 'http://store.digitalriver.com/store?Action=buy&Env=BASE&Locale=de_DE&ProductID=5458374100&SiteID=defaults'
 rx6800xtMB = 'http://store.digitalriver.com/store?Action=buy&Env=BASE&Locale=de_DE&ProductID=5496921500&SiteID=defaults'
@@ -13,6 +12,8 @@ rx6700xt = 'http://store.digitalriver.com/store?Action=buy&Env=BASE&Locale=de_DE
 rx6800 = 'http://store.digitalriver.com/store?Action=buy&Env=BASE&Locale=de_DE&ProductID=5458374000&SiteID=defaults'
 rx6900xt = 'http://store.digitalriver.com/store?Action=buy&Env=BASE&Locale=de_DE&ProductID=5458374200&SiteID=defaults'
 cpu = 'http://store.digitalriver.com/store?Action=buy&Env=BASE&Locale=de_DE&ProductID=5450881700&SiteID=defaults'
+
+
 
 def checkURL(dom, pos):
     if dom.__contains__("CAT_000015"):
@@ -23,9 +24,10 @@ def checkURL(dom, pos):
         #applescript = applescript + 'with icon caution buttons {"OK"}'
         #subprocess.call("osascript -e '{}'".format(applescript), shell=True)
         print(pos + "available")
+        t.cancel()
 
 def startAll():
-  threading.Timer(5.0, startAll).start()
+  t.start()
   checkURL(BeautifulSoup(requests.get(rx6800xt).text, "html.parser").text, "RX_6800_XT ")
   checkURL(BeautifulSoup(requests.get(rx6800xtMB).text, "html.parser").text, "RX_6000XT_mb ")
   checkURL(BeautifulSoup(requests.get(rx6800).text, "html.parser").text, "RX_6800 ")
@@ -34,6 +36,7 @@ def startAll():
   checkURL(BeautifulSoup(requests.get(cpu).text, "html.parser").text, "Ryzen_5_5600x ")
   print("--------------")
 
+t = threading.Timer(5.0, startAll)
 startAll()
 
 
